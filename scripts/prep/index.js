4ba95b741ad20a6b22f9e2d0a36656e6879ae469
Rich Harris <richard.a.harris@gmail.com>
2017-06-04 10:37:33 -0400

    build codemirror

--- scripts/prep/index.js
+++ scripts/prep/index.js
@@ -4,4 +4,5 @@ Promise.resolve()
 	.then( () => require( './build-components.js' )() )
 	.then( () => require( './build-css.js' )() )
 	.then( () => require( './build-examples.js' )() )
+	.then( () => require( './build-repl.js' )() )
 	.then( () => require( './build-guide.js' )() );
