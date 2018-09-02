606b887a6027c20f5a893e4a186b959b3331eb9b
Rich Harris <richard.a.harris@gmail.com>
2018-09-02 16:17:46 -0400

    some progress

--- routes/repl/_Editor/codemirror.js
+++ routes/repl/_Editor/codemirror.js
@@ -1,5 +1,7 @@
-const CodeMirror = require('codemirror');
-require('codemirror/mode/javascript/javascript.js');
-require('codemirror/lib/codemirror.css');
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/javascript/javascript.js';
+import 'codemirror/lib/codemirror.css';
 
-module.exports = CodeMirror;
+console.log({CodeMirror})
+
+export default CodeMirror;
