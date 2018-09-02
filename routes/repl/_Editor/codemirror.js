df3cc814b6c306dbece94860b3292a37182feb45
Rich Harris <richard.a.harris@gmail.com>
2018-09-02 16:24:33 -0400

    various fixes

--- routes/repl/_Editor/codemirror.js
+++ routes/repl/_Editor/codemirror.js
@@ -2,6 +2,4 @@ import CodeMirror from 'codemirror';
 import 'codemirror/mode/javascript/javascript.js';
 import 'codemirror/lib/codemirror.css';
 
-console.log({CodeMirror})
-
 export default CodeMirror;
