433ae874693b9c84cb271e2382ab897c4902a11f
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-02-07 13:41:53 +0100

    Accept circleci build numbers

--- src/routes/repl/index.svelte
+++ src/routes/repl/index.svelte
@@ -1,17 +1,19 @@
 <script context="module">
 	export function preload() {
-		return this.fetch(`api/examples.json`).then(r => r.json()).then(examples => ({ examples }));
+		return this.fetch(`api/examples.json`)
+			.then(r => r.json())
+			.then(examples => ({ examples }));
 	}
 </script>
 
 <script>
 	import Input from './_Input/index.svelte';
 	import Output from './_Output/index.svelte';
-	import {dirname, resolve} from './_utils/path';
-	import {supportsCodeSplitting, supportsInput} from './_utils/rollupVersion';
-    import { onMount } from 'svelte';
-    import { stores } from '@sapper/app';
-    import { get } from 'svelte/store';
+	import { dirname, resolve } from './_utils/path';
+	import { supportsCodeSplitting, supportsInput } from './_utils/rollupVersion';
+	import { onMount } from 'svelte';
+	import { stores } from '@sapper/app';
+	import { get } from 'svelte/store';
 
 	export let examples = [];
 	let output = [];
@@ -30,96 +32,113 @@
 	let rollup;
 	let error;
 
-	function getUrlRollupVersion() {
-	  if (typeof window === 'undefined') return null;
-	  const versionMatch = /version=([^&]+)/.exec(window.location.search);
-	  return versionMatch && versionMatch[1];
+	const atob =
+		typeof window === 'undefined'
+			? base64 => Buffer.from(base64, 'base64').toString()
+			: window.atob;
+
+	async function getRollupUrl(query) {
+		if (query.circleci) {
+			const artifacts = await (await fetch(
+				`https://circleci.com/api/v1.1/project/github/rollup/rollup/${query.circleci}/artifacts`
+			)).json();
+			const artifact =
+				Array.isArray(artifacts) &&
+				artifacts.find(artifact => artifact.path.endsWith('rollup.browser.js'));
+			if (!artifact) {
+				throw new Error('Invalid CircleCI build number.');
+			}
+			return artifact.url;
+		} else {
+			return query.version
+				? `https://unpkg.com/rollup@${query.version}/dist/rollup.browser.js`
+				: 'https://unpkg.com/rollup/dist/rollup.browser.js';
+		}
 	}
 
-	const urlRollupVersion = getUrlRollupVersion();
+	async function loadRollup(query) {
+		const url = await getRollupUrl(query);
+		return new Promise(async (fulfil, reject) => {
+			const script = document.createElement('script');
+			script.src = url;
+			script.onload = () => {
+				fulfil(window.rollup);
+			};
+			script.onerror = reject;
+			document.querySelector('head').appendChild(script);
+		});
+	}
 
-	const atob = typeof window ==='undefined'
-		? base64 => Buffer.from(base64, 'base64').toString()
-		: window.atob;
+	onMount(async () => {
+		const { query } = get(stores().page);
+		try {
+			if (query.shareable) {
+				const json = decodeURIComponent(atob(query.shareable));
+				({ modules, options, example: selectedExample } = JSON.parse(json));
+				input.$set({ modules, selectedExample });
+			} else if (query.gist) {
+				const result = await (await fetch(`https://api.github.com/gists/${query.gist}`, {
+					method: 'GET',
+					headers: { Accept: 'application/vnd.github.v3+json' }
+				})).json();
+				const entryModules = query.entry ? query.entry.split(',') : [];
+				modules = [result.files['main.js'] || { filename: 'main.js', content: '' }]
+					.concat(
+						Object.keys(result.files)
+							.filter(fileName => fileName !== 'main.js')
+							.map(fileName => result.files[fileName])
+					)
+					.map(module => ({
+						name: module.filename,
+						code: module.content,
+						isEntry: entryModules.indexOf(module.filename) >= 0
+					}));
+			} else {
+				selectedExample = '00';
+			}
+		} catch (err) {
+			console.error(err);
+			selectedExample = '00';
+		}
 
-	function loadRollup () {
-		const url = urlRollupVersion ?
-			`https://unpkg.com/rollup@${urlRollupVersion}/dist/rollup.browser.js` :
-			'https://unpkg.com/rollup/dist/rollup.browser.js';
-
-    	return new Promise(( fulfil, reject ) => {
-    		const script = document.createElement('script');
-    		script.src = url;
-    		script.onload = () => {
-    			fulfil(window.rollup);
-    		};
-    		script.onerror = reject;
-    		document.querySelector('head').appendChild(script);
-    	});
-    }
-
-    onMount(async () => {
-    	const {query} = get(stores().page);
-        try {
-        	if ( query.shareable ) {
-        		const json = decodeURIComponent(atob(query.shareable));
-        		({modules, options, example: selectedExample} = JSON.parse(json));
-        		input.$set({modules, selectedExample});
-        	} else if ( query.gist ) {
-        		const result = await (await fetch(`https://api.github.com/gists/${query.gist}`, {
-        			method: 'GET',
-        			headers: { Accept: 'application/vnd.github.v3+json' }
-        		})).json();
-        		const entryModules = query.entry ? query.entry.split(',') : [];
-        		modules = [result.files['main.js'] || { filename: 'main.js', content: '' }]
-        			.concat(Object.keys(result.files)
-        				.filter(fileName => fileName !== 'main.js')
-        				.map(fileName => result.files[fileName])
-        			).map(module => ({
-        				name: module.filename,
-        				code: module.content,
-        				isEntry: entryModules.indexOf(module.filename) >= 0
-        			}));
-        	} else {
-        		selectedExample = '00';
-        	}
-        } catch (err) {
-        	console.error(err);
-        	selectedExample = '00';
-        }
-
-		rollup = await loadRollup();
-		codeSplitting = rollup && supportsCodeSplitting(rollup.VERSION);
-		return requestBundle();
-    });
+		try {
+			rollup = await loadRollup(query);
+			codeSplitting = rollup && supportsCodeSplitting(rollup.VERSION);
+			return requestBundle();
+		} catch (e) {
+			error = e;
+		}
+	});
 
 	$: {
-	    if (selectedExample) {
-	        updateSelectedExample();
-	    }
+		if (selectedExample) {
+			updateSelectedExample();
+		}
 	}
 
 	function updateSelectedExample() {
-		fetch(`api/examples/${selectedExample}.json`).then(r => r.json()).then(example => {
-        	modules = example.modules;
-        	selectedExampleModules = modules.map(module => ({...module}))
-        });
-        input.$set({modules, selectedExample});
+		fetch(`api/examples/${selectedExample}.json`)
+			.then(r => r.json())
+			.then(example => {
+				modules = example.modules;
+				selectedExampleModules = modules.map(module => ({ ...module }));
+			});
+		input.$set({ modules, selectedExample });
 	}
 
 	$: {
-	  if (modules) {
-	    requestDebouncedBundle();
-	  }
+		if (modules) {
+			requestDebouncedBundle();
+		}
 	}
 
 	$: {
-	  if (options) {
-	    requestBundle();
-	  }
+		if (options) {
+			requestBundle();
+		}
 	}
 
-    // TODO instead of debouncing, we should bundle in a worker
+	// TODO instead of debouncing, we should bundle in a worker
 	let bundleDebounceTimeout;
 	function requestDebouncedBundle() {
 		clearTimeout(bundleDebounceTimeout);
@@ -130,24 +149,29 @@
 	async function requestBundle() {
 		if (!modules.length || !rollup) return;
 		if (bundlePromise) {
-		  await bundlePromise;
+			await bundlePromise;
 		}
-		bundlePromise = bundle().then(() => bundlePromise = null);
+		bundlePromise = bundle().then(() => (bundlePromise = null));
 	}
 
-	async function bundle () {
+	async function bundle() {
 		console.clear();
 		console.log(`running Rollup version %c${rollup.VERSION}`, 'font-weight: bold');
 		if (selectedExample && selectedExampleModules.length) {
-		  if (modules.length !== selectedExampleModules.length || selectedExampleModules.some((module, index) => {
-		    const currentModule = modules[index];
-		    return currentModule.name !== module.name ||
-		        currentModule.code !== module.code ||
-		        currentModule.isEntry !== module.isEntry;
-		  })) {
-		    selectedExample = null;
-		    selectedExampleModules = [];
-		  }
+			if (
+				modules.length !== selectedExampleModules.length ||
+				selectedExampleModules.some((module, index) => {
+					const currentModule = modules[index];
+					return (
+						currentModule.name !== module.name ||
+						currentModule.code !== module.code ||
+						currentModule.isEntry !== module.isEntry
+					);
+				})
+			) {
+				selectedExample = null;
+				selectedExampleModules = [];
+			}
 		}
 
 		updateUrl();
@@ -160,44 +184,46 @@
 
 		warnings = [];
 		const inputOptions = {
-			plugins: [{
-				resolveId ( importee, importer ) {
-					if ( !importer ) return importee;
-					if ( importee[0] !== '.' ) return false;
+			plugins: [
+				{
+					resolveId(importee, importer) {
+						if (!importer) return importee;
+						if (importee[0] !== '.') return false;
 
-					let resolved = resolve(dirname(importer), importee).replace(/^\.\//, '');
-					if ( resolved in moduleById ) return resolved;
+						let resolved = resolve(dirname(importer), importee).replace(/^\.\//, '');
+						if (resolved in moduleById) return resolved;
 
-					resolved += '.js';
-					if ( resolved in moduleById ) return resolved;
+						resolved += '.js';
+						if (resolved in moduleById) return resolved;
 
-					throw new Error(`Could not resolve '${importee}' from '${importer}'`);
-				},
-				load: function ( id ) {
-					return moduleById[id].code;
+						throw new Error(`Could not resolve '${importee}' from '${importer}'`);
+					},
+					load: function(id) {
+						return moduleById[id].code;
+					}
 				}
-			}],
-			onwarn ( warning ) {
+			],
+			onwarn(warning) {
 				warnings.push(warning);
 
 				console.group(warning.loc ? warning.loc.file : '');
 
 				console.warn(warning.message);
 
-				if ( warning.frame ) {
+				if (warning.frame) {
 					console.log(warning.frame);
 				}
 
-				if ( warning.url ) {
+				if (warning.url) {
 					console.log(`See ${warning.url} for more information`);
 				}
 
 				console.groupEnd();
 			}
 		};
-		if ( codeSplitting ) {
+		if (codeSplitting) {
 			inputOptions.input = modules
-				.filter(( module, index ) => index === 0 || module.isEntry)
+				.filter((module, index) => index === 0 || module.isEntry)
 				.map(module => module.name);
 		} else {
 			inputOptions[supportsInput(rollup.VERSION) ? 'input' : 'entry'] = 'main.js';
@@ -206,42 +232,47 @@
 		try {
 			const generated = await (await rollup.rollup(inputOptions)).generate(options);
 
-			if ( codeSplitting ) {
+			if (codeSplitting) {
 				output = generated.output;
 				error = null;
 			} else {
-			    output = [generated];
+				output = [generated];
 				error = null;
 			}
 		} catch (err) {
 			error = err;
-			if ( error.frame ) console.log(error.frame);
+			if (error.frame) console.log(error.frame);
 			setTimeout(() => {
 				throw error;
 			});
 		}
 	}
 
-	function updateUrl () {
-    	if ( typeof history === 'undefined' ) return;
+	function updateUrl() {
+		if (typeof history === 'undefined') return;
 
-    	const params = {};
-    	if ( typeof rollup !== 'undefined' ) {
-    		params.version = rollup.VERSION;
-    	} else if ( urlRollupVersion ) {
-    		params.version = urlRollupVersion;
-    	}
+		const params = {};
+		const { query } = get(stores().page);
+		if (query.circleci) {
+			params.circleci = query.circleci;
+		} else if (typeof rollup !== 'undefined') {
+			params.version = rollup.VERSION;
+		} else if (query.version) {
+			params.version = query.version;
+		}
 
-    	const json = JSON.stringify({
-    		modules,
-    		options,
-    		example: selectedExample
-    	});
-    	params.shareable = btoa(encodeURIComponent(json));
-    	const queryString = Object.keys(params).map(key => `${key}=${params[key]}`).join('&');
-    	const url = `/repl/?${queryString}`;
-    	window.history.replaceState({}, '', url);
-    }
+		const json = JSON.stringify({
+			modules,
+			options,
+			example: selectedExample
+		});
+		params.shareable = btoa(encodeURIComponent(json));
+		const queryString = Object.keys(params)
+			.map(key => `${key}=${params[key]}`)
+			.join('&');
+		const url = `/repl/?${queryString}`;
+		window.history.replaceState({}, '', url);
+	}
 </script>
 
 <style>
@@ -249,7 +280,8 @@
 		height: calc(100% - 3.6em);
 	}
 
-	.left, .right {
+	.left,
+	.right {
 		width: 100%;
 		padding: 1em;
 	}
@@ -265,7 +297,8 @@
 		margin-bottom: 1em;
 	}
 
-	:global(button):hover, :global(button):active {
+	:global(button):hover,
+	:global(button):active {
 		background-color: #eaeaea;
 	}
 
@@ -294,9 +327,9 @@
 		background-color: #eaeaea;
 	}
 
-
 	@media (min-width: 45rem) {
-		.left, .right {
+		.left,
+		.right {
 			width: 50%;
 			height: 100%;
 			float: left;
@@ -305,22 +338,26 @@
 	}
 </style>
 
-<div class='repl'>
-	<div class='left'>
+<div class="repl">
+	<div class="left">
 		<h2>ES6 modules go in...</h2>
-		<div class='input'>
-		    <Input {examples} {codeSplitting} bind:selectedExample bind:modules bind:this={input}/>
+		<div class="input">
+			<Input {examples} {codeSplitting} bind:selectedExample bind:modules bind:this="{input}" />
 		</div>
 	</div>
-	<div class='right'>
-	    <h2>...{#if output.length > 1}chunks come{:else}bundle comes{/if} out</h2>
-		<div class='output'>
-			<Output bind:options {output} {error} {warnings} waiting={!rollup}/>
+	<div class="right">
+		<h2>
+			...
+			{#if output.length > 1}chunks come{:else}bundle comes{/if}
+			out
+		</h2>
+		<div class="output">
+			<Output bind:options {output} {error} {warnings} waiting="{!rollup}" />
 		</div>
 	</div>
 </div>
 
 <!-- trick Sapper into generating example JSON files -->
 {#each examples as example}
-    <a hidden href="api/examples/{example.id}.json">{example.title}</a>
+	<a hidden href="api/examples/{example.id}.json">{example.title}</a>
 {/each}
