08c7c9d61be2bccb4b1c00a85198c0270ab414c9
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-03-05 07:13:00 +0100

    Use a shared SVG image

--- src/routes/guide/[lang]/_data.js
+++ src/routes/guide/[lang]/_data.js
@@ -50,7 +50,7 @@ function create_guide(lang) {
 		const renderer = new marked.Renderer();
 		renderer.heading = (text, level, raw, slugger) => {
 			const id = slugger.slug(raw);
-			return `<h${level} id="${id}"><a class="anchor" href="guide/en/#${id}"><svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>${text}</h${level}>`;
+			return `<h${level} id="${id}"><a class="anchor" href="guide/en/#${id}"><img src="/images/anchor.svg" alt=""></a>${text}</h${level}>`;
 		};
 		const html = marked(content, { renderer })
 			.replace(/<p>(<a class='open-in-repl'[\s\S]+?)<\/p>/g, '$1')
