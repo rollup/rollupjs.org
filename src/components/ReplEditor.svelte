7cc10d1170d787288016fecc382197b5c5fae38a
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2022-09-19 13:03:31 +0200

    Error highlighting

--- src/components/ReplEditor.svelte
+++ src/components/ReplEditor.svelte
@@ -2,7 +2,6 @@
 	import { onDestroy, onMount } from 'svelte';
 	import rollupOutput from '../stores/rollupOutput';
 	import { getFileNameFromMessage } from '../helpers/messages';
-	import { createEditor } from '../helpers/editor';
 
 	export let code;
 	export let readonly = false;
@@ -11,67 +10,32 @@
 	let previousCode = code;
 	let editorContainer;
 	let editor;
+	let addWarningsEffect;
 
 	$: if (moduleName && editor) {
-		// TODO Lukas remove overlay
 		const { error, warnings } = $rollupOutput;
-		// TODO Lukas add overlay
-		// if (error) {
-		// 	addOverlay([error], 'rollup-error');
-		// } else {
-		// 	addOverlay(warnings, 'rollup-warning');
-		// }
-	}
-
-	// TODO Lukas remove/rework
-	function addOverlay(messages, type) {
-		const relevantMessages = messages.filter(
-			message => message.loc && getFileNameFromMessage(message) === moduleName
-		);
-		if (relevantMessages.length) {
-			const lines = {};
-			for (const {
-				loc: { line, column }
-			} of relevantMessages) {
-				if (lines[line]) {
-					lines[line].push(column);
-				} else {
-					lines[line] = [column];
-				}
-			}
-			editor.addOverlay({
-				name: 'locations',
-				token(stream) {
-					const line = lines[stream.lineOracle.line + 1];
-					if (line) {
-						let i = 0;
-						while (typeof line[i] === 'number' && stream.pos > line[i]) {
-							i++;
-						}
-						if (typeof line[i] !== 'number') {
-							stream.skipToEnd();
-						} else if (stream.pos < line[i]) {
-							stream.pos = line[i];
-						} else {
-							stream.pos++;
-							return type;
-						}
-					} else {
-						stream.skipToEnd();
-					}
-				}
-			});
+		if (error) {
+			addMarkers([error], 'error');
+		} else {
+			addMarkers(warnings, 'warning');
 		}
 	}
 
+	function addMarkers(messages, type) {
+		const relevantMessages = messages.filter(
+			message => typeof message.pos === 'number' && getFileNameFromMessage(message) === moduleName
+		);
+		editor.dispatch({ effects: [addWarningsEffect.of({ messages: relevantMessages, type })] });
+	}
+
 	onMount(async () => {
-		const { createEditor } = await import('../helpers/editor.js');
+		const { createEditor, addWarnings } = await import('../helpers/editor.js');
+		addWarningsEffect = addWarnings;
 		editor = createEditor(
 			editorContainer,
 			code,
 			({ changedRanges, state: { doc } }) => {
 				if (changedRanges.length) {
-					// TODO Lukas removeOverlay
 					code = doc.toString();
 					previousCode = code;
 				}
@@ -99,44 +63,4 @@
 		width: 100%;
 		height: 100%;
 	}
-
-	.codemirror-container :global(.cm-editor .cm-content),
-	.codemirror-container :global(.cm-editor .cm-gutters) {
-		font-family: Inconsolata, monospace;
-		font-size: 16px;
-		line-height: 1.2;
-		font-weight: 400;
-	}
-
-	.codemirror-container :global(.cm-editor .cm-content) {
-		color: #333;
-		height: 100%;
-	}
-
-	.codemirror-container :global(.cm-editor .cm-gutters) {
-		color: #999;
-	}
-
-	.codemirror-container :global(.cm-editor) {
-		outline: none;
-	}
-
-	.codemirror-container :global(.cm-gutters) {
-		border-right: 1px solid #eee;
-	}
-
-	/* TODO Lukas overlay styling */
-	/*:global(.CodeMirror) :global(.cm-overlay.cm-rollup-warning) {*/
-	/*	background-color: var(--warning-background);*/
-	/*	color: var(--warning-color);*/
-	/*	padding: 1px;*/
-	/*	margin: -1px;*/
-	/*}*/
-
-	/*:global(.CodeMirror) :global(.cm-overlay.cm-rollup-error) {*/
-	/*	background-color: var(--error-background);*/
-	/*	color: var(--error-color);*/
-	/*	padding: 1px;*/
-	/*	margin: -1px;*/
-	/*}*/
 </style>
--- src/helpers/editor.js
+++ src/helpers/editor.js
@@ -1,28 +1,83 @@
-import {
-	dropCursor,
-	EditorView,
-	highlightSpecialChars,
-	keymap,
-	lineNumbers
-} from '@codemirror/view';
-import { EditorState } from '@codemirror/state';
-import {
-	bracketMatching,
-	defaultHighlightStyle,
-	foldKeymap,
-	indentOnInput,
-	syntaxHighlighting
-} from '@codemirror/language';
+import { Decoration, dropCursor, EditorView, keymap, lineNumbers } from '@codemirror/view';
+import { EditorState, StateEffect, StateField } from '@codemirror/state';
+import { defaultHighlightStyle, indentOnInput, syntaxHighlighting } from '@codemirror/language';
 import { defaultKeymap, history, historyKeymap } from '@codemirror/commands';
-import { highlightSelectionMatches } from '@codemirror/search';
-import {
-	autocompletion,
-	closeBrackets,
-	closeBracketsKeymap,
-	completionKeymap
-} from '@codemirror/autocomplete';
-import { lintKeymap } from '@codemirror/lint';
+import { closeBrackets, closeBracketsKeymap } from '@codemirror/autocomplete';
 import { javascript } from '@codemirror/lang-javascript';
+
+export const addWarnings = StateEffect.define();
+
+const warningsField = StateField.define({
+	create() {
+		return Decoration.none;
+	},
+	update(warnings, transaction) {
+		let hasWarning = false;
+		for (let e of transaction.effects) {
+			if (e.is(addWarnings)) {
+				if (!hasWarning) {
+					hasWarning = true;
+					warnings = Decoration.none;
+				}
+				warnings = warnings.update({
+					add: e.value.messages
+						.sort((a, b) => a.pos - b.pos)
+						.map(({ pos, message }) =>
+							Decoration.mark({
+								attributes: { class: `cm-rollup-${e.value.type}`, title: message }
+							}).range(pos, pos + 1)
+						)
+				});
+			}
+		}
+		return warnings;
+	},
+	provide: field => EditorView.decorations.from(field)
+});
+
+const marks = {
+	warning: Decoration.mark({ attributes: { class: 'cm-rollup-warning', title: 'oooo' } }),
+	error: Decoration.mark({ class: 'cm-rollup-error' })
+};
+
+const editorFont = {
+	fontFamily: 'Inconsolata, monospace',
+	fontSize: '16px',
+	lineHeight: '1.2',
+	fontWeight: '400'
+};
+
+const theme = EditorView.baseTheme({
+	'.cm-rollup-warning': {
+		backgroundColor: 'var(--warning-background)',
+		color: 'var(--warning-color)',
+		padding: '1px',
+		margin: '-1px'
+	},
+	'.cm-rollup-error': {
+		backgroundColor: 'var(--error-background)',
+		color: 'var(--error-color)',
+		padding: '1px',
+		margin: '-1px'
+	},
+	'.cm-scroller': {
+		fontFamily: 'Inconsolata, monospace',
+		fontSize: '16px',
+		lineHeight: '1.2',
+		fontWeight: '400'
+	},
+	'.cm-content': {
+		color: '#333',
+		height: '100%'
+	},
+	'.cm-gutters': {
+		color: '#999',
+		borderRight: '1px solid #eee'
+	},
+	'&.cm-editor.cm-focused': {
+		outline: 'none'
+	}
+});
 
 export const createEditor = (parent, doc, onUpdate, readonly) =>
 	new EditorView({
@@ -30,27 +85,18 @@ export const createEditor = (parent, doc, onUpdate, readonly) =>
 		doc,
 		extensions: [
 			lineNumbers(),
-			highlightSpecialChars(),
 			history(),
 			dropCursor(),
 			indentOnInput(),
 			syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-			bracketMatching(),
 			closeBrackets(),
-			autocompletion(),
-			highlightSelectionMatches(),
-			keymap.of([
-				...closeBracketsKeymap,
-				...defaultKeymap,
-				...historyKeymap,
-				...foldKeymap,
-				...completionKeymap,
-				...lintKeymap
-			]),
+			keymap.of([...closeBracketsKeymap, ...defaultKeymap, ...historyKeymap]),
 			javascript(),
 			EditorState.readOnly.of(readonly),
 			EditorView.lineWrapping,
 			EditorState.tabSize.of(2),
-			EditorView.updateListener.of(onUpdate)
+			EditorView.updateListener.of(onUpdate),
+			warningsField,
+			theme
 		]
 	});
